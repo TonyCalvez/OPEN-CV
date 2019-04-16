2b430953186a639c8c6962c6a7390de8dbe14688
Tony CALVEZ <tony.calvez@hotmail.fr>
2019-04-16 17:23:08 +0200

    Update COURS_3.2_Video.py

--- COURS_3.2_Video.py
+++ COURS_3.2_Video.py
@@ -1,25 +1,26 @@
 import cv2
-import numpy as np
 
 def main():
 
-    cap = cv2.VideoCapture(1)
+    def cb_seuillage(x):
+        ret, binary = cv2.threshold(thresh1,cv2.getTrackbarPos("SeuilT","Binary"),255,cv2.THRESH_BINARY_INV)
+        cv2.imshow("Binary",binary)
 
-    while (True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        # Display the resulting frame
-        cv2.imshow('frame', gray)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # When everything done, release the capture
-    cap.release()
+    img = cv2.imread('voilier_oies_blanches.jpg')
+    thresh1 = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    seuil = 100
+    cv2.namedWindow("Binary",0)
+    cv2.createTrackbar("SeuilT","Binary",seuil,255,cb_seuillage)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-if __name__ == "_main_":
-      main()
+
+#3b img_ds bruitee
+# -> image binaire est tres bruitee
+# -> 2 solutions pour ameliorer le resultat
+#       -> a) prefiltrage de img_ds.jpg avant binaritation
+#       -> b) post-traitement de l'image binaire a l'aide d'operateurs de morphologie mathematique (CF TE4)
+
+
+if __name__ == "__main__":
+    main()
