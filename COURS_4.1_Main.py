8c3b032fafb788c3a1a87a9af219214a1d991649
Tony CALVEZ <tony.calvez@hotmail.fr>
2019-04-29 12:03:24 +0200

    Update COURS_4.1_Main.py

--- COURS_4.1_Main.py
+++ COURS_4.1_Main.py
@@ -117,12 +117,33 @@ def morphological(img):
     cv2.imshow('Gradient:', gradient)
 
 def etiquageconnexes(img):
+    ret, labels = cv2.connectedComponents(img)
+    imshow_components(labels)
     pass
+
+
+def imshow_components(labels):
+    # Map component labels to hue val
+    label_hue = np.uint8(179*labels/np.max(labels))
+    blank_ch = 255*np.ones_like(label_hue)
+    labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
+
+    # cvt to BGR for display
+    labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
+
+    # set bg label to black
+    labeled_img[label_hue==0] = 0
+
+    cv2.imshow('labeled.png', labeled_img)
+    cv2.waitKey()
+
+
 
 if __name__ == "__main__":
     img = niveaudegris('voilier_oies_blanches.jpg')
     img = imagebinariseinv(img,255)
-    plt.imshow(img, plt='jet')
+    img= cv2.connectedComponents(img)
+    img = etiquageconnexes(img)
 
     cv2.waitKey(0)
-    cv2.destroyAllWindows()  
+    cv2.destroyAllWindows()
